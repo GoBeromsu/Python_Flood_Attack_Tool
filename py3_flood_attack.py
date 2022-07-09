7fe51bb120cc0ad121f885e773808255f9fa4ce3
goberomsu <310o@naver.com>
2022-07-09 23:48:29 +0900

    Fix : script structure

--- py3_flood_attack.py
+++ py3_flood_attack.py
@@ -7,16 +7,16 @@ from argparse import ArgumentParser
 # ICMP/UDP/TCP Flood Attack Tool
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--type", "-T", help="attack number of repetition")
-    parser.add_argument("--target", "-t", help="target IP address")
-    parser.add_argument("--port", "-p", help="target port number")
-    parser.add_argument("--repeat", "-r", help="attack number of repetition")
+    parser.add_argument("--SynFlood", "-s", help="Syn Flood Attack")
+    parser.add_argument("--UDPFlood", "-u", help="UDP Flood Attack")
+    parser.add_argument("--ICMPFlood", "i", help="ICMP Flood Attack")
+    parser.add_argument("--target", "-t", required=True, help="target IP address")
+    parser.add_argument("--port", "-p", default=80, help="target port number")
+    parser.add_argument(
+        "--repeat", "-r", default=100, help="attack number of repetition"
+    )
 
     args = parser.parse_args()
-    if args.type:
-        pass
-    else:
-        print("Attack Type is Missing")
     if args.target:
         print(args.target)
     else:
@@ -28,7 +28,9 @@ def main():
     if args.repeat:
         pass
     
-
+    if not SynFlood and not UDPFlood and not ICMPFlood:
+        pass
+    
 # def set
 
 main()
